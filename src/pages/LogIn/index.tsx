c05f1065db6549a95e59b983a1947a7b76b7be68
Gyumong <rlaalsrb3559@naver.com>
2021-04-09 21:40:05 +0900

    초기세팅

---
+++ src/pages/LogIn/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LoginBlock } from "./styles";
+const LogIn = () => {
+  return (
+    <LoginBlock>
+      <h2>로그인</h2>
+    </LoginBlock>
+  );
+};
+
+export default LogIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SignUpBlock } from "./styles";
+const SignUp = () => {
+  return (
+    <SignUpBlock>
+      <h2>회원가입</h2>
+    </SignUpBlock>
+  );
+};
+
+export default SignUp;
